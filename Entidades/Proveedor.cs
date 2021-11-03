d4b3701d185565a7a5fcec372c33b1d345e7bbbf
FranC <73913524+FranciscoCrestanello@users.noreply.github.com>
2021-11-03 12:00:58 -0300

    Add files via upload

--- Entidades/Proveedor.cs
+++ Entidades/Proveedor.cs
@@ -8,8 +8,9 @@ namespace Entidades
 {
     public class Proveedor
     {
-        private String CodProveedor;
+        private int CodProveedor;
         private String RazonSocial;
+        private String Marca;
         private String Direccion;
         private String Ciudad;
         private String Provincia;
@@ -18,19 +19,60 @@ namespace Entidades
         private String Contacto;
         private String Email;
         private String Web;
+        private int CBU;
 
-        public Proveedor()
-        { }
+        public Proveedor() { }
 
-        public string CodProveedor1 { get => CodProveedor; set => CodProveedor = value; }
-        public string RazonSocial1 { get => RazonSocial; set => RazonSocial = value; }
-        public string Direccion1 { get => Direccion; set => Direccion = value; }
-        public string Ciudad1 { get => Ciudad; set => Ciudad = value; }
-        public string Provincia1 { get => Provincia; set => Provincia = value; }
-        public int Cuit1 { get => Cuit; set => Cuit = value; }
-        public int Telefono1 { get => Telefono; set => Telefono = value; }
-        public string Contacto1 { get => Contacto; set => Contacto = value; }
-        public string Email1 { get => Email; set => Email = value; }
-        public string Web1 { get => Web; set => Web = value; }
+        public Proveedor(int CodProveedor_, String RazonSocial_, String Marca, String Direccion_, String Ciudad_, String Provincia_, int Cuit_, int Telefono_, String Contacto_, String Email_, String Web_, int CBU_)
+        {
+            this.CodProveedor = CodProveedor_;
+            this.RazonSocial = RazonSocial_;
+            this.Marca = Marca;
+            this.Direccion = Direccion_;
+            this.Ciudad = Ciudad_;
+            this.Provincia = Provincia_;
+            this.Cuit = Cuit_;
+            this.Telefono = Telefono_;
+            this.Contacto = Contacto_;
+            this.Email = Email_;
+            this.Web = Web_;
+            this.CBU = CBU_;
+        }
+
+        public int GetCodProveedor() { return CodProveedor; }
+        public void SetCodProveedor(int CodProveedor) { this.CodProveedor = CodProveedor; }
+
+        public string GetRazonSocial() { return RazonSocial; }
+        public void SetRazonSocial(string RazonSocial) { this.RazonSocial = RazonSocial; }
+
+        public string GetMarca() { return Marca; }
+        public void SetMarca(string Marca) { this.Marca = Marca; }
+
+        public string GetDireccion() { return Direccion; }
+        public void SetDireccion(string Direccion) { this.Direccion = Direccion; }
+
+        public string GetCiudad() { return Ciudad; }
+        public void SetCiudad(string Ciudad) { this.Ciudad = Ciudad; }
+
+        public string GetProvincia() { return Provincia; }
+        public void SetProvincia(string Provincia) { this.Provincia = Provincia; }
+
+        public int GetCuit() { return Cuit; }
+        public void SetCuit(int Cuit) { this.Cuit = Cuit; }
+
+        public int GetTelefono() { return Telefono; }
+        public void SetTelefono(int Telefono) { this.Telefono = Telefono; }
+
+        public string GetContacto() { return Contacto; }
+        public void SetContacto(string Contacto) { this.Contacto = Contacto; }
+
+        public string GetEmail() { return Email; }
+        public void SetEmail(string Email) { this.Email = Email; }
+
+        public string GetWeb() { return Web; }
+        public void SetWeb(string Web) { this.Web = Web; }
+
+        public int GetCBU() { return CBU; }
+        public void SetCBU(int CBU) { this.CBU = CBU; }
     }
 }
--- Entidades/Venta.cs
+++ Entidades/Venta.cs
@@ -9,16 +9,53 @@ namespace Entidades
     public class Venta
     {
         private int IdFactura;
-        private String IdCliente;
+        private int IdCliente;
         private DateTime FechaVenta;
-        private float PrecioTotal;
+        private decimal TotalFactura;
 
-        public Venta()
-        { }
+        public Venta() { }
+        public Venta(int IdFactura_, int IdCliente_, DateTime FechaVenta_, decimal TotalFactura_)
+        {
+            this.IdFactura = IdFactura_;
+            this.IdCliente = IdCliente_;
+            this.FechaVenta = FechaVenta_;
+            this.TotalFactura = TotalFactura_;
+        }
+        /// SETS
+        public void SetIdFactura(int IdFactura)
+        {
+            this.IdFactura = IdFactura;
+        }
+        public void SetIdCliente(int IdCliente)
+        {
+            this.IdCliente = IdCliente;
+        }
+        public void SetFechaVenta(DateTime FechaVenta)
+        {
+            this.FechaVenta = FechaVenta;
+        }
+        public void SetTotalFactura(decimal TotalFactura)
+        {
+            this.TotalFactura = TotalFactura;
+        }
+        /// GETS
 
-        public int IdFactura1 { get => IdFactura; set => IdFactura = value; }
-        public string IdCliente1 { get => IdCliente; set => IdCliente = value; }
-        public DateTime FechaVenta1 { get => FechaVenta; set => FechaVenta = value; }
-        public float PrecioTotal1 { get => PrecioTotal; set => PrecioTotal = value; }
+        public int GetIdFactura()
+        {
+            return IdFactura;
+        }
+        public int GetIdCliente()
+        {
+            return IdCliente;
+        }
+        public DateTime GetFechaVenta()
+        {
+            return FechaVenta;
+        }
+        public decimal GetTotalFactura()
+        {
+            return TotalFactura;
+        }
+
     }
 }
